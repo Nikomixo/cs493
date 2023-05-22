bdefcf77cd0945fe410524c515b919eed0bef4e1
Nikomixo <kevint0218@outlook.com>
2023-05-22 00:46:33 -0700

    updated schema

--- Project 3/data/schema.sql
+++ Project 3/data/schema.sql
@@ -10,30 +10,30 @@ CREATE TABLE `businesses` (
   `subcategory` varchar(255) NOT NULL,
   `website` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `ownerid` mediumint NOT NULL,
+  `ownerId` mediumint NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `reviews` (
   `id` mediumint NOT NULL AUTO_INCREMENT,
-  `userid` mediumint NOT NULL,
-  `businessid` mediumint NOT NULL,
+  `userId` mediumint NOT NULL,
+  `businessId` mediumint NOT NULL,
   `dollars` mediumint NOT NULL,
   `stars` mediumint NOT NULL,
   `review` text,
   PRIMARY KEY (`id`),
   KEY `businessid` (`businessid`),
-  CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`businessid`) REFERENCES `businesses` (`id`)
+  CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`businessId`) REFERENCES `businesses` (`id`)
 );
 
 CREATE TABLE `photos` (
   `id` mediumint NOT NULL AUTO_INCREMENT,
-  `userid` mediumint NOT NULL,
-  `businessid` mediumint NOT NULL,
+  `userId` mediumint NOT NULL,
+  `businessId` mediumint NOT NULL,
   `caption` text,
   PRIMARY KEY (`id`),
-  KEY `businessid` (`businessid`),
-  CONSTRAINT `photos_ibfk_1` FOREIGN KEY (`businessid`) REFERENCES `businesses` (`id`)
+  KEY `businessId` (`businessId`),
+  CONSTRAINT `photos_ibfk_1` FOREIGN KEY (`businessId`) REFERENCES `businesses` (`id`)
 );
 
 CREATE TABLE `users` (
